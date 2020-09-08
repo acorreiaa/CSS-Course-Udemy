6f56d377d2a417643c6070aeefbb8ace962e1c41
Amanda <amandaluana777@gmail.com>
2020-09-08 07:37:39 -0300

    correction  modal-js/mobile header responsive

--- shared.js
+++ shared.js
@@ -1,7 +1,7 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
-var no = document.querySelector('.modal__action--negative');
+var modalNoButton = document.querySelector('.modal__action--negative');
 //console.dir(no);
 
 //console.dir(selectPlanButtons);
@@ -11,7 +11,13 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
         backdrop.style.display = 'block';
     });
 }
-no.addEventListener('click', function () {
+
+backdrop.addEventListener('click', function () {
+    modal.style.display = 'none';
+    backdrop.style.display = 'none';
+} );
+
+modalNoButton.addEventListener('click', function () {
     modal.style.display = 'none';
     backdrop.style.display = 'none';
 });
