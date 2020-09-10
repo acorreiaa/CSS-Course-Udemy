f980c858cbe988d4bc84f6e085b5c6240653bdb0
Amanda <amandaluana777@gmail.com>
2020-09-10 07:46:52 -0300

    all pages with menu

--- shared.js
+++ shared.js
@@ -18,17 +18,27 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
 }
 
 backdrop.addEventListener('click', function () {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
-    mobileNav.style.display = 'none';
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
 });
 
-modalNoButton.addEventListener('click', function () {
-    modal.style.display = 'none';
-    backdrop.style.display = 'none';
-});
+if (modalNoButton) {
+modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal() {
+    //backdrop.style.display = 'none';
+    //modal.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove ('open');
+}
 
 toggleButton.addEventListener('click', function() {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    //mobileNav.style.display = 'block';
+    //backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 })
